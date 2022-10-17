3300d3be60731c82c930ff0679c87640d2fda1c3
MZC01-HTCHOI <albino@kakao.com>
2022-10-17 16:41:42 +0900

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,6 @@
 echo 'env settings ... '
 
-apt install virtualenv
-
-VENV_PATH="${SCRIPT_DIR}/venv"
-if [ ! -d ${VENV_PATH} ]; then
-  virtualenv -p python3.6 "${VENV_PATH}"
-  source ${VENV_PATH}/bin/activate
-  pip install -r requirements.txt
-else
-  echo "${VENV_PATH} already exists. Skipping virtual environment setup."
-  source ${VENV_PATH}/bin/activate
-fi
-
+pip install -r requirements.txt
 
 echo 'download mask dataset ... '
 python data_load.py
